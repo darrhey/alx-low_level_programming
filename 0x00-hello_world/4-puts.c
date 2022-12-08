7c1c84c7495e7c2388197d6c86eef15c1d372080
Elegbede Oludare <elegbededare@gmail.com>
2022-12-08 03:06:33 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
-int main(void){
-  puts("\"Programming is like building a multilingual puzzle");
-  return(0);
+
+/**
+ * main - Program entry point
+ *
+ * Return: 0 0n success. Error code otherwise
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
