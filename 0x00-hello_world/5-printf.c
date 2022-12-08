9abbb54ba6b4eabec401e45c17f33b7fd39b7bf9
Elegbede Oludare <elegbededare@gmail.com>
2022-12-08 02:21:46 -0800

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,8 @@
 #include<stdio.h>
+/**
+ * Accepts no argument and return 0
+ * Only prints the strings in printf function
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
