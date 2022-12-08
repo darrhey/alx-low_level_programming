3acddc2a3ce26013f4ec338b296a7f58c69040ce
Elegbede Oludare <elegbededare@gmail.com>
2022-12-08 04:56:17 -0800

    UNIX is basically a simple ...

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - Program entry point
+ *
+ * Return: 0 0n success. Error code otherwise
+ */
+int main(void)
+{
+int i;
+char c;
+long int li;
+long long int lli;
+float f;
+fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (0);
+}
