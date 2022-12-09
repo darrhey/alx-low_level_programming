6e247fa741853c98ce68b53a97a87bfe1fe69e2b
Elegbede Oludare <elegbededare@gmail.com>
2022-12-09 04:19:42 -0800

    a-z

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,10 +6,11 @@
  */
 int main()
 {
-  char a;
-  for (a='a';a<='z';a++){
-    putchar(a);
-  }
-  putchar('\n');
-  return (0);
+char a;
+for (a='a';a<='z';a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
 }
