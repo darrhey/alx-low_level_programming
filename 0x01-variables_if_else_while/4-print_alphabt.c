af3152ea68d1a3e29080c6321205cf3af274da4e
Elegbede Oludare <elegbededare@gmail.com>
2022-12-09 03:01:42 -0800

    alphabets skip q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Program entry point
+ *
+ * Return: 0 on success. Error code otherwise
+ */
+int main(void)
+{
+  char a;
+  for (a='a';a<='z';a++){
+    if ((c=='e') || (c=='q')){
+      c +=1;
+    } else{
+      putchar(a);
+    }
+  }
+  putchar('\n');
+  return (0);
+}
