4553ff9293aa6a4df04627312f178e1df84bdca4
Elegbede Oludare <elegbededare@gmail.com>
2022-12-09 02:16:35 -0800

    Write lowercase alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - Program entry point
+ *
+ * Return: 0 on success. Error code otherwise
+ */
+int main(void)
+{
+  char a;
+  for (a='a'; a<=z ; a++){
+    putchar(str(a)+"\n");
+  }
+  return (0);
+}
