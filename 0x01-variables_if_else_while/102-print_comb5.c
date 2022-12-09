86ec516d94cef97377cc067208e40b2ef478bf6e
Elegbede Oludare <elegbededare@gmail.com>
2022-12-09 12:45:05 -0800

    Four combo

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+/**
+ * main - Program entry point
+ *
+ * Return: 0 on success. Error code otherwise
+ */
+int main(void)
+{
+int a;
+int b;
+int c;
+for (a = 0; a <= 7; a++)
+{
+for (b = a + 1; b <= 8; b++)
+{
+for (c = a + 2; c <= 9; c++)
+{
+if ((a == b) || (b == c) || (a == c))
+{
+continue;
+}
+else
+{
+putchar(48 + a);
+putchar(48 + b);
+putchar(48 + c);
+}
+if ((a == 7) && (b == 8) && (c == 9))
+{
+continue;
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
