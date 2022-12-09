3384f37bbeaa9b9ca14fe2aea57e1bc9b556db57
Elegbede Oludare <elegbededare@gmail.com>
2022-12-09 04:26:02 -0800

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,15 +4,17 @@
  *
  * Return: 0 on success. Error code otherwise
  */
-int main()
+int main(void)
 {
-  char a;
-  for (a='a';a<='z';a++){
-    putchar(a);
-  }
-  for (a='A';a<='Z';a++){
-    putchar(a);
-  }
-  putchar('\n');
-  return (0);
+char a;
+for (a = 'a'; a <= 'z'; a++)
+{
+putchar(a);
+}
+for (a = 'A'; a <= 'Z'; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
 }
