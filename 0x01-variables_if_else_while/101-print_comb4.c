47e2aaabdeefba2b65bbdf659cc70b71326fad34
Elegbede Oludare <elegbededare@gmail.com>
2022-12-09 12:33:27 -0800

    Three combinations

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+/**
+ * main - Program entry point
+ *
+ * Return: 0 on success. Error code otherwise
+ */
+int main(void)
+{
+int a;
+int b;
+int c;
+for (a = 0; a < 9; a++)
+{
+for (b = a + 1; b <= 9; b++)
+{
+for (c = a + 2; c <= 9; c++)
+{
+if ((a == b) || (b == c) || (a == c))
+{
+continue;
+}
+else
+{
+putchar(48 + a);
+putchar(48 + b);
+putchar(48 + c);
+}
+if ((a == 7) && (b == 8) && (c == 9))
+{
+continue;
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
