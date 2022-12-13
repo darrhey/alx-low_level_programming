a96016f68ab61f9e92405e6b3a9dd3b122ae61b1
Elegbede Oludare <elegbededare@gmail.com>
2022-12-13 09:20:07 -0800

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include<stdlib.h>
+#include "main.h"
+/**
+ * declaration of function that checks for alphabet characters.
+ *
+ * Return: 1 if lowercase but 0 if otherwise.
+ */
+
+int _abs(int)
+{
+return abs(int);
+}
+
