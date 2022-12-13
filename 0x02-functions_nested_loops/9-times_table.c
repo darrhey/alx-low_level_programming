70afe653c3f7a21aa5cd271e75fa9685cd1b6e9c
Elegbede Oludare <elegbededare@gmail.com>
2022-12-13 15:39:08 -0800

    Times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include <unistd.h>
+#include<stdlib.h>
+#include "main.h"
+/**
+ * 
+ */
+
+void times_table(void)
+{
+int n;
+int m;
+int p;
+n = 0;
+while (n <= 9)
+{
+m = 0;
+while (m <= 9)
+{
+p = n * m;
+if (p >= 10)
+{
+_putchar((p / 10) + 48);
+_putchar((p % 10) + 48);
+}
+else
+{
+_putchar(p + 48);
+}
+_putchar(',');
+_putchar(' ');
+m++;
+}
+n++;  
+}
+}
