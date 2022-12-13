e3eedd35adfdd94b79b0fd3b77460912b91d19f2
Elegbede Oludare <elegbededare@gmail.com>
2022-12-13 09:29:03 -0800

    Last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include<stdlib.h>
+#include "main.h"
+/**
+ * print_last_digit - Takes any integer to return its last digit.
+ */
+
+int print_last_digit(int)
+{
+int n;
+int last;
+last = n%10;
+return (last);
+}
+
