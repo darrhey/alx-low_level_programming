942188d11c440e77802e5143fcccbbc45cb733f8
Elegbede Oludare <elegbededare@gmail.com>
2022-12-13 05:41:31 -0800

    Check if alphabets and return 1

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * declaration of function that checks for alphabet character _isalpha to return 1.
+ *
+ * Return: 1 if lowercase but 0 if otherwise.
+ */
+
+int _isalpha(int c)
+{
+if (isalpha(c) == 1)
+{
+return 1;
+}
+else
+{
+return 0;
+}
