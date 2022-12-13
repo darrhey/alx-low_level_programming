9443cc601f6d38f45a1bebf6487ba9e40734dc30
Elegbede Oludare <elegbededare@gmail.com>
2022-12-13 08:17:22 -0800

    Alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -21,3 +21,27 @@ _putchar("\n");
 repeat_times++;
 }
 }
+#include <unistd.h>
+#include "main.h"
+/**
+ * main - Program entry point
+ *
+ * Return: 0 on success. Error code otherwise
+ */
+
+void print_alphabet_x10(void)
+{
+int count;
+int m;
+int repeat_times;
+count = 0;
+for (repeat_times = 0; repeat_times < 10; repeat_times++)
+{
+while (count < 26)
+{
+m = _putchar(97 + count);
+count += 1;
+}
+write(1, &m, 1);
+}
+}
