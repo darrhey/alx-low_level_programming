d0ebdf0828d1697777db81fde88a2b562b749b44
Elegbede Oludare <elegbededare@gmail.com>
2022-12-13 08:02:32 -0800

    Alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * main - Program entry point
+ *
+ * Return: 0 on success. Error code otherwise
+ */
+
+int main(void)
+{
+print_alphabet();
+return (0);
+}
+
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
+
+void print_alphabet(void)
+{
+int count;
+int m;
+count = 0;
+while (count < 26)
+{
+m = _putchar(97+count);
+count += 1;
+}
+write(1, &m, 1);
+write(1, "\n", 1);
+}
