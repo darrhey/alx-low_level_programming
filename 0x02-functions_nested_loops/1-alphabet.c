4d256352440644ddbab85dacbe9623c48abd326b
Elegbede Oludare <elegbededare@gmail.com>
2022-12-13 13:24:04 -0800

    Alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,12 +9,11 @@
 void print_alphabet(void)
 {
 int count;
-int m;
 count = 0;
 while (count < 26)
 {
-m = _putchar(97 + count);
+_putchar(98 + count);
 count += 1;
 }
-write(1, &m, 1);
+_putchar(98 + count);
 }
