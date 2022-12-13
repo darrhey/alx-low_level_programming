8bdf954dff8710b97d45f46df1180089f23c870b
Elegbede Oludare <elegbededare@gmail.com>
2022-12-13 07:49:09 -0800

    10x alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * main - Program entry point
+ *
+ * Return: 0 on success. Error code otherwise
+ */
+
+void print_alphabet_x10(void)
+{
+int repeat_times;
+repeat_times = 0;
+while (repeat_times < 10)
+{
+for (letter = 97; letter <= 122; letter++)
+{
+_putchar(letter)
+}
+_putchar("\n");
+repeat_times++;
+}
+}
