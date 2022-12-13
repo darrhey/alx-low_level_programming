a38e0afe0a61ba06e3ccef3a3280542968e02370
Elegbede Oludare <elegbededare@gmail.com>
2022-12-13 03:36:05 -0800

    Print _putchar followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * main - Program entry point
+ *
+ * Return: 0 on success. Error code otherwise
+ */
+
+int main(void)
+{
+write(1,"_putchar\n",9);
+return (0);
+}
