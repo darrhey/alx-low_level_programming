5cb048e3bb6b08a020eb1f3a1373b4b9c7740d4b
Elegbede Oludare <elegbededare@gmail.com>
2022-12-13 13:41:21 -0800

    Alphabetx10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,10 +10,10 @@ void print_alphabet_x10(void)
 {
 int count;
 int repeat_times;
-count = 0;
 repeat_times = 0;
 while (repeat_times < 10)
 {
+count = 0;
 while (count < 26)
 {
 _putchar(97 + count);
