f7a3b8313acf8e3de9c4a7bd79fa01f7bf93a15c
Elegbede Oludare <elegbededare@gmail.com>
2022-12-13 19:39:48 -0800

    n times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,44 @@
+#include <unistd.h>
+#include<stdlib.h>
+#include "main.h"
+/**
+ *
+ */
+
+void print_times_table(int n)
+{
+int a;
+int m;
+int p;
+a = 0;
+while (a <= n)
+{
+m = 0;
+while (m <= n)
+{
+p = a *m;
+if (p >= 10)
+{
+_putchar((p / 10) + 48);
+_putchar((p % 10) + 48);
+}
+else if (m == 0)
+{
+_putchar(p + 48);
+}
+else
+{
+_putchar(' ');
+_putchar(p + 48);
+}
+if (m < n)
+{
+_putchar(',');
+_putchar(' ');
+}
+m++;
+}
+_putchar('\n');
+a++;
+}
+}
