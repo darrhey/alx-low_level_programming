b592b011928349f11b6f8fa3331e032a72a95048
Elegbede Oludare <elegbededare@gmail.com>
2022-12-13 20:59:47 -0800

    Natural numbers

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include<stdio.h>
 #include<stdlib.h>
 #include "main.h"
 /**
@@ -7,7 +8,7 @@
  * Return 0 on success and otherwise if error.
  */
 
-int main()
+int main(void)
 {
 int three;
 int five;
@@ -29,10 +30,6 @@ sum_of_5 = five;
 five += 5;
 }
 total = sum_of_3 + sum_of_5;
-_putchar((total / 10000) + 48);
-_putchar(((total % 10000) / 1000) + 48);
-_putchar((((total % 10000) % 1000) / 100) + 48);
-_putchar(((((total % 10000) % 1000) % 100) / 10) + 48);
-_putchar(((((total % 10000) % 1000) % 100) % 10) + 48);
+printf("%d", total);
 return (0);
 }
