6d6876913f58b861f94c753da5a4246da8fd9e78
Elegbede Oludare <elegbededare@gmail.com>
2022-12-13 22:07:16 -0800

    Fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include "main.h"
+/**
+ * Main - Program entry point
+ *
+ * Return 0 on success and otherwise if error.
+ */
+
+int main(void)
+{
+int num1;
+int num2;
+int num3;
+int item;
+num1 = 1;
+num2 = 2;
+item = 50;
+while (item > 0)
+{
+num3 = num1 + num2;
+num1 = num2;
+num2 = num3;
+printf("%d, ",num3);
+item--;
+}
+return (0);
+}
