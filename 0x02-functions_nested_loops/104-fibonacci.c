3fef5eb10be3e7be7b5ccdf5b0c35e11a1476a04
Elegbede Oludare <elegbededare@gmail.com>
2022-12-13 22:52:18 -0800

    Fibo

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include "main.h"
+/**
+ * Main - Program entry point
+ *
+ * Return 0 on success and otherwise if error.
+ */
+
+int main(void)
+{
+unsigned long int num1;
+unsigned long int num2;
+unsigned long int num3;
+unsigned long int item;
+num1 = 1;
+num2 = 2;
+item = 98;
+printf("%lu, %lu", num1, num2);
+while (item > 2)
+{
+num3 = num1 + num2;
+num1 = num2;
+num2 = num3;
+printf(", %lu", num3);
+item--;
+}
+printf("\n");
+return (0);
+}
