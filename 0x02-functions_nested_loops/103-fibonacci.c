bf4db098be75ec46c0fb55aa979aa25c34c4223b
Elegbede Oludare <elegbededare@gmail.com>
2022-12-13 22:42:15 -0800

    Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,34 @@
+#include <unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include "main.h"
+/**
+ * Main - Program entry point
+ *
+ * Return 0 on success and otherwise if error.
+ */
+
+int main(void)
+{
+unsigned long int num1;
+unsigned long int num2;
+unsigned long int num3;
+unsigned long int max;
+unsigned long int sum;
+num1 = 1;
+num2 = 2;
+max = 4000000;
+sum = 2;
+while (num3 < max)
+{
+num3 = num1 + num2;
+num1 = num2;
+num2 = num3;
+if ((num3 % 2) == 0)
+{
+sum += num3;
+}
+}
+printf("%lu\n",sum);
+return (0);
+}
