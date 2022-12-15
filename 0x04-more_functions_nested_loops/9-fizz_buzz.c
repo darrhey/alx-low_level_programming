e23e4d95c5eb1226168b42132f73df5e488fd5a9
Elegbede Oludare <elegbededare@gmail.com>
2022-12-15 14:11:06 -0800

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+int num;
+for (num = 1; num <= 100; num++)
+{
+if (((num % 3) == 0) && ((num % 5) == 0))
+{
+printf("FizzBuzz ");
+}
+else if ((num % 3) == 0)
+{
+printf("Fizz ");
+}
+else if ((num % 5) == 0)
+{
+printf("Buzz ");
+}
+else
+{
+printf("%d ", num);
+}
+}
+}
