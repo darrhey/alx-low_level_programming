3f2d15f775325a80f2fa0ee4589377559097886a
Elegbede Oludare <elegbededare@gmail.com>
2022-12-15 13:32:04 -0800

    Print lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,7 +3,6 @@
 /**
  * print_line - draws a straight line in the terminal
  * @n: parameter for number of lines
- * Return: 0 on success. Error code otherwise
  */
 
 void print_line(int n)
@@ -14,5 +13,4 @@ _putchar(95);
 n -= 1;
 }
 _putchar('\n');
-return (0);
 }
