bd0d25a4826dcdf4946be31c115422d53708c5a3
Elegbede Oludare <elegbededare@gmail.com>
2022-12-15 13:30:19 -0800

    Print lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,7 +1,7 @@
 #include <unistd.h>
 #include "main.h"
 /**
- * print_numbers - draws a straight line in the terminal
+ * print_line - draws a straight line in the terminal
  * @n: parameter for number of lines
  * Return: 0 on success. Error code otherwise
  */
@@ -14,4 +14,5 @@ _putchar(95);
 n -= 1;
 }
 _putchar('\n');
+return (0);
 }
