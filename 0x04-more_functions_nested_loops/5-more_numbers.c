0d8964040fbd24f75c4aa8f3ccb068bae15c8b3f
Elegbede Oludare <elegbededare@gmail.com>
2022-12-14 23:57:07 -0800

    0 to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * more_numbers - display numbers from 0 to 14
+ * Return: 0 on success. Error code otherwise
+ */
+
+void more_numbers(void)
+{
+int count;
+count = 0;
+while (count <= 14)
+{
+if (count >= 10)
+{
+_putchar((count / 10) + 48);
+_putchar((count % 10) + 48);
+}
+else
+{
+_putchar(count + 48);
+count += 1;
+}
+}
+}
