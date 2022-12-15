43729900d76c201afab2df9e97b18a0a7e6074d7
Elegbede Oludare <elegbededare@gmail.com>
2022-12-14 23:22:39 -0800

    Multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include<ctype.h>
+#include "main.h"
+/**
+ * mul - returns product of two numbers
+ * @a: first operand
+ * @b: second operand
+ * Return: the product of the two operand
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
