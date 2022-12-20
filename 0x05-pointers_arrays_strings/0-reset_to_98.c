3f38aa22113331aa40517e27644e758d9bddf215
Elegbede Oludare <elegbededare@gmail.com>
2022-12-19 18:07:34 -0800

    Second commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_square - prints square(s), followed by new line
+ * @size: parameter for number of square(s)
+ */
+
+void print_square(int size)
+{
+int a;
+int b;
+a = 0;
+if (size > 0)
+{
+while (a < size)
+{
+b = 0;
+while (b < size)
+{
+_putchar('#');
+b += 1;
+}
+_putchar('\n');
+a += 1;
+}
+}
+else
+{
+_putchar('\n');
+}
+}
