35f98e26ef6a0c051e41d4636b54835f5b13454c
Elegbede Oludare <elegbededare@gmail.com>
2022-12-24 00:32:56 -0800

    Reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse, followed by a newline
+ * @s: string to be printed in reverse
+ */
+
+void print_rev(char *s)
+{
+puts(strrev(s));
+}
