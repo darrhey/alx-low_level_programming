379e207e78a3df5b2baabc11706f258fe3c233af
Elegbede Oludare <elegbededare@gmail.com>
2022-12-23 23:33:47 -0800

    length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string to be calculated
+ */
+
+int _strlen(char *s)
+{
+return (size_t strlen(s));
+}
