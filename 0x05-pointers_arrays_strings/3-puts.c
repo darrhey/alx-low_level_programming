ca6674eb0eec9608499734d9f34e6c39f8c920f8
Elegbede Oludare <elegbededare@gmail.com>
2022-12-24 00:21:31 -0800

    print with newline

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts - prints a string, followed by a newline
+ * @str: string to be printed
+ */
+
+void _puts(char *str)
+{
+puts(str);
+}
