ac0d9e05b4d30bc71d4e34b79232d7d1749d1f61
Elegbede Oludare <elegbededare@gmail.com>
2022-12-23 23:00:06 -0800

    reset to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,31 +1,14 @@
 #include <unistd.h>
 #include "main.h"
 /**
- * print_square - prints square(s), followed by new line
- * @size: parameter for number of square(s)
+ * reset_to_98 - resets the address of n to 98
+ * @n: parameter to be reset
  */
 
-void print_square(int size)
+void reset_to_98(int *n)
 {
-int a;
-int b;
-a = 0;
-if (size > 0)
-{
-while (a < size)
-{
-b = 0;
-while (b < size)
-{
-_putchar('#');
-b += 1;
-}
-_putchar('\n');
-a += 1;
-}
-}
-else
-{
-_putchar('\n');
-}
+  int n;
+  int *p;
+  p = &n;
+  *p = 98;
 }
