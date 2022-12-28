93b8439a171ba3d61e612fae4f6710031d1b6ac4
Elegbede Oludare <elegbededare@gmail.com>
2022-12-28 08:54:42 +0100

    Half the lies

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string
+ * @str: string to be printed alternatively
+ */
+
+void puts2(char *str)
+{
+int lenght;
+int a;
+lenght = strlen(str);
+for (a = 0; a <= lenght; a += 2)
+{
+_putchar(str[a]);
+}
+_putchar('\n');
+}
