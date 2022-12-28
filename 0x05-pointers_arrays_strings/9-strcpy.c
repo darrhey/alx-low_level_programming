0b8f646a9272e5141d13aa1682f6a5f9e0223b9e
Elegbede Oludare <elegbededare@gmail.com>
2022-12-28 09:07:41 +0100

    copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * strcpy - copies the string pointed to by src to dest
+ * @dest: where string will be copied
+ * @src: string to be copied
+ * Return: return the dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+strcpy(dest, src);
+return (dest);
+}
