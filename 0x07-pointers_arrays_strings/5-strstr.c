28f8791a5b78a4a1ce54783b4b9d4f96020e8201
Elegbede Oludare <elegbededare@gmail.com>
2023-01-03 09:00:49 +0100

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,37 +1,22 @@
-#include <unistd.h>
-#include <stdio.h>
 #include <string.h>
 #include "main.h"
 /**
- * print_diagsums - prints the sum of diagonals of square matrix
- * @a: the square matrix to be analysed
- * @size: the size of the square matrix
+ * _strstr - locates a substring
+ * @haystack: the string to be searched
+ * @needle: the substring to be checked
+ * Return: pointer to the substring but NULL when absent
  */
 
-void print_diagsums(int *a, int size)
+char *_strstr(char *haystack, char *needle)
 {
-int i;
-int j;
-int k;
-int sum1;
-int sum2;
-sum1 = 0;
-sum2 = 0;
-k = 0;
-for (i = 0; i < size; i++)
+char* q;
+q = strstr(haystack, needle);
+if (q)
 {
-for (j = 0; j < size; j++)
-{
-if (i == j)
-{
-sum1 += a[k];
+return (needle);
 }
-if ((i + j) == (size - 1))
+else
 {
-sum2 += a[k];
+return (NULL);
 }
-k++;
-}
-}
-printf("%d, %d\n", sum1, sum2);
 }
