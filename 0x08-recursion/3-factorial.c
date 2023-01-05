526b56ccd745a68e0bdf545a79d94f475bb55e5a
Elegbede Oludare <elegbededare@gmail.com>
2023-01-05 10:29:08 +0100

    factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,13 +7,16 @@
 
 int factorial(int n)
 {
-if (n<0)
+while (n > 0)
 {
-return (-1);
-}
 if ((n <= 1) && (n > 0))
 {
 return (1);
 }
 return (factorial(n) * factorial(n-1));
+}
+if (n < 0)
+{
+return -1;
+}
 }
