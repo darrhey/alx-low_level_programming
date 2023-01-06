c18b6aa5234aebc4561aed6a9eb0815fe408e4f5
Elegbede Oludare <elegbededare@gmail.com>
2023-01-06 15:38:08 +0100

    What is my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - prints its name followed by newline
+ * @argc: argument count
+ * @argcv: array of pointers to arg strings
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
