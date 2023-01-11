da5414bc345815b18166a85a3c67114d6de3fd4a
Elegbede Oludare <elegbededare@gmail.com>
2023-01-11 20:10:29 +0100

    free space

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * free_grid - free memory
+ * @grid: pointer to grid
+ * @height: rows of array
+ * Return: free up memory
+*/
+
+void free_grid(int **grid, int height)
+{
+int i;
+if (grid == NULL || grid == 0)
+{
+return (NULL);
+}
+for (i = 0; i < height; i++)
+{
+free(grid[i]);
+}
+free(grid);
+}
