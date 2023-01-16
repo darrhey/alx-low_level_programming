51da260e2cc1b5c79d9e1e236594b0fbd764abe6
Elegbede Oludare <elegbededare@gmail.com>
2023-01-16 14:05:31 +0100

    Poppy and function prototype

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,5 @@
-#include "main.h"
+#ifndef DOG_H
+#define DOG_H
 
 #include <string.h>
 
@@ -16,3 +17,18 @@ char *name;
 float age;
 char *owner;
 };
+
+/**
+ * dog_t - Typedef for struct dog
+ **/
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+int _putchar(char c);
+
+#endif /* MAIN_H */
