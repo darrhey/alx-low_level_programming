a6166f3be6780b3647f6b5b585bb67627c2ccff2
Elegbede Oludare <elegbededare@gmail.com>
2023-01-16 11:10:43 +0100

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - cntains all the features of dog
+ * name - feature of char* type
+ * age - feature of float type
+ * owner - feature of char* type
+ * Description - this create a struct dog with some members
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
