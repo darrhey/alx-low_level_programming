ed5fdf42e4e04c3306a59539a251126933aa8ccd
Elegbede Oludare <elegbededare@gmail.com>
2023-01-16 21:26:41 +0100

    free dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees dog
+ * @d - dog to be freed
+ **/
+
+void free_dog(dog_t *d)
+{
+if (d == NULL)
+{
+return;
+}
+free(d->name);
+free(d->age);
+free(d->owner);
+free(d);
+}
