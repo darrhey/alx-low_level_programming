596f6edb720f42a96e2d9556defbfa41d2f54e5c
Elegbede Oludare <elegbededare@gmail.com>
2023-01-19 13:36:55 +0100

    variables

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,10 +18,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
   va_start(ap, n);
 
-  for (i = 0; i < n; i++)
+  for (i = 1; i < n; i++)
     {
       printf("%d", va_arg(ap, int));
-      if (i != (n - 1) && separator != NULL)
+      if ( separator != NULL)
 	printf("%s", separator);
     }
   printf("\n");
