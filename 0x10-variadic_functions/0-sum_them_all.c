49f379089ce9e8ce193ec7aca36dddc54a907092
Elegbede Oludare <elegbededare@gmail.com>
2023-01-19 12:37:16 +0100

    add all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,18 +10,20 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
+va_list ap;
+va_start(ap, n);
+
 if (n == 0)
 {
 return (0);
 }
-va_list ap;
-va_start(ap, n);
 int i = 0;
 int sum = 0;
 for (i = 0; i < n; i++)
 {
 sum += va_arg(ap, int);
 }
+
 va_end(n);
 return (sum);
 }
