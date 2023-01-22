c9869f5a3ee8ead07148569fb4d343291ee08867
Elegbede Oludare <elegbededare@gmail.com>
2023-01-22 19:46:33 +0100

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,17 +7,18 @@
 
 void print_to_98(int n)
 {
-if (n <  98)
+if (n <=  98)
 {
 while (n <= 98)
 {
-if (n < 10){
-_putchar(n+48);
+if (n < 10)
+{
+_putchar(n + '0');
 }
 else
 {
-_putchar(n/10 + 48);
-_putchar(n%10 + 48);
+  _putchar((n/10) + '0');
+  _putchar((n%10) + '0');
 }
 _putchar(',');
 _putchar(' ');
@@ -30,18 +31,18 @@ while (n >= 98)
 {
 if (n >= 100)
 {
-_putchar(n/100 + 48);
-_putchar((n%100)/10 + 48);
-_putchar((n%100)%10 + 48);
+  _putchar((n / 100) + '0');
+  _putchar(((n % 100) / 10) + '0');
+  _putchar(((n % 100) % 10) + '0');
 }
 else
 {
-_putchar(n/10 + 48);
-_putchar(n%10 + 48);
+  _putchar((n / 10) + '0');
+  _putchar((n  % 10) + '0');
 }
 _putchar(',');
 _putchar(' ');
-n++;
+n--;
 }
 }
 }
