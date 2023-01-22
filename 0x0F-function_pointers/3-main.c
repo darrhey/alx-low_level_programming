85de390c65dd610932a73a2aad52bbee250d91a0
Elegbede Oludare <elegbededare@gmail.com>
2023-01-22 23:52:27 +0100

    main menu

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - prints result of simple arithmetic operations
+ * @argc: number of arguments supplied
+ * @argv: array of pointer to parameters
+ * Return: 0
+ **/
+
+int main(int argc, char *argv[])
+{
+int i, j;
+char *op;
+if (argc != 4)
+{
+printf("Error\n");
+}
+i = atoi(argv[1]);
+op = argv[2];
+j = atoi(argv[3]);
+if (get_op_func(op) == NULL)
+{
+printf("Error\n");
+}
+if ((*op == "/" && j == 0) || (*op == "%" && j == 0))
+{
+printf("Error\n");
+}
+printf("%d\n",get_op_func(op)(i, j));
+return (0);
+}
