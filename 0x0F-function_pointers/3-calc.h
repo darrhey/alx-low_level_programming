ff12ed74e569d04fed567375f8b5d311dc235c49
Elegbede Oludare <elegbededare@gmail.com>
2023-01-22 23:28:01 +0100

    calc prototypes

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,3 +1,6 @@
+#ifndef CALC_H
+#define CALC_H
+
 /**
  * struct op - Struct op
  *
@@ -10,3 +13,12 @@ typedef struct op
   char *op;
   int (*f)(int a, int b);
 } op_t;
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
+
+#endif /*CALC_H*/
