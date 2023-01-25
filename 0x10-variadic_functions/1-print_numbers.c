48020353ec2986cc75458be34b3f6fe200a0f6b5
Elegbede Oludare <elegbededare@gmail.com>
2023-01-25 18:36:09 +0100

    print number

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -3,19 +3,18 @@
 #include "variadic_functions.h"
 
 /**
- * print_numbers - prints numbers, followed by a new line
- * @separator - const char type
- * @n - const unsigned integer type
- * @... - elipsis
+ * print_numbers: prints numbers, followed by a new line
+ * @separator: const char type
+ * @n: const unsigned integer type
  *
  * Return: nothing;
  **/
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+unsigned int i;
 va_list list;
 va_start(list, n);
-unsigned int i;
 for (i = 0; i < n; i++)
 {
 printf("%d", (va_arg(list, int)));
