bc874a16ca01164a695b377062edacbe6defa67a
Elegbede Oludare <elegbededare@gmail.com>
2023-01-25 18:46:57 +0100

    print strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -4,19 +4,18 @@
 
 /**
  * print_strings - prints strings, followed by a new line
- * @separator - const char type
- * @n - const unsigned integer type
- * @... - elipsis
+ * @separator: const char type
+ * @n: const unsigned integer type
  *
  * Return: nothing;
  **/
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
+unsigned int i;
+int j;
 va_list list;
 va_start(list, n);
-int i;
-int j;
 for (i = 0; i < n; i++)
 {
 j = va_arg(list, int);
