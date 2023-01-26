e53ae33dda499b36159dc187dc4a08d85079b4c8
Elegbede Oludare <elegbededare@gmail.com>
2023-01-26 19:04:24 +0100

    add node end

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_node_end - adds a new node at the end of a list_t
+ * @head: pointer to the head
+ * @str: string to be added
+ * Return: the address of the new element or NULL if failed
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+list_t *tmp;
+size_t len;
+len = 0;
+tmp = malloc(sizeof(list_t));
+if (tmp == NULL)
+{
+return (NULL);
+}
+while (str[len])
+{
+len++;
+}
+tmp->len = len;
+tmp->str = strdup(str);
+if (*head == NULL)
+{
+tmp->next = NULL;
+}
+else
+{
+tmp->next = *head;
+}
+*head = tmp;
+return (*head);
+}
