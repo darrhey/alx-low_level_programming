bfd93f62b1ee9b57a9341751ff18300f51969056
Elegbede Oludare <elegbededare@gmail.com>
2023-01-26 19:52:19 +0100

    add node end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,49 +9,40 @@
  * Return: the address of the new element or NULL if failed
  */
 
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
-list_t *tmp;
-if (*head == NULL)
-{
-*head = tmp_node(str);
-return (*head);
-}
-if (((*head)->next) == NULL)
-{
-tmp = tmp_node(str);
-tmp->next = (*head)->next;
-(*head)->next = tmp;
-}
-else
-{
-add_node_end(&(*head)->next, str);
-}
-return (*head);
-}
-
-/**
- * tmp_nocde - creates a new node
- * @str: string to add
- *
- * Return: pointer to a list_t
- */
-list_t *tmp_node(const char *str)
-{
-list_t *tmp;
+list_t *add, *new, *last;
 size_t len;
-tmp = malloc(sizeof(list_t));
-if (tmp == NULL)
+new = malloc(sizeof(list_t));
+if (new == NULL)
 {
 return (NULL);
 }
-len = 0;
-while (str[len])
+add = strdup(str);
+if (str == NULL)
+{
+free(new);
+return (NULL);
+}
+for (len = 0; str[len];)
 {
 len++;
 }
-tmp->len = len;
-tmp->str = strdup(str);
-tmp->next = NULL;
-return (tmp);
+new->str = add;
+new->len = len;
+new->next = NULL;
+if (*head == NULL)
+{
+*head = new;
+}
+else
+{
+last = *head;
+while (last->next != NULL)
+{
+last = last->next;
+}
+last->next = new;
+}
+return (*head);
 }
