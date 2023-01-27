959e6d7445bd9ef06c775af8df11aa9d3498f692
Elegbede Oludare <elegbededare@gmail.com>
2023-01-27 14:43:28 +0100

    prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+int sqrt_odd(int odd, int n);
+
+/**
+ * is_prime_number - checks for prime number
+ * @n: int to be checked
+ * Return: 1 if a prime number, 0 if otherwise
+ */
+
+int is_prime_number(int n)
+{
+if (n <= 1)
+{
+return (0)
+}
+return (prime_num(n, (n - 1)));
+}
+
+/**
+ * prime_num - confirm prime numbers
+ * @odd: number to be checked
+ * @n: number to check with
+ * Return: 1 if prime number 0 if otherwise
+ */
+int prime_num(int i, int j)
+{
+if (j == 1)
+return (1);
+else if (i % j == 0)
+return (0);
+else if (i % j != 0)
+return (prime_num(i, (j - 1)));
+return (0);
+}
