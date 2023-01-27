ec8fad95c0d830e6ce220d43a866971e579baf96
Elegbede Oludare <elegbededare@gmail.com>
2023-01-27 15:47:47 +0100

    wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,40 +1,24 @@
 #include "main.h"
 
-int find(char *s, int alpha, int beta);
-int _strlen_recursion(char *s);
 /**
  * wildcmp - compares two strings
- * @s1: string to be checked
- * @s2: string to be checked
+ * @s1: string to be compared
+ * @s2: string to be compared
  * Return: 1 if a string is palindrome, 0 if otherwise
  */
 
 int wildcmp(char *s1, char *s2)
 {
-int i, j;
-i = _strlen_recursion(s1);
-j = _strlen_recursion(s2);
-if (s1[i] != s2[j])
-return (0);
-if (s1[i] == s2[j] && i == j)
+if (*s1 == '\0' && *s2 == '\0')
 return (1);
-else if (s1[i] == s2[j] && i > j)
+if (*s1 == *s2)
+return (wildcmp((s1 + 1), (s2 + 1)));
+if (*s2 == '*')
+{
+if (*(s2 + 1) == '*')
+return (wildcmp(s1, (s2 + 1)));
+if (wildcmp((s1 + 1), s2) || wildcmp(s1, (s2 + 1)))
 return (1);
-else
-return (wildcmp(s, (alpha + 1), (beta + 1)));
-}
-
-/**
- * _strlen_recursion - returns length of a string
- * @s: string to be measured
- * Return: length of string s
- */
-
-int _strlen_recursion(char *s)
-{
-if (*s)
-{
-return (1 + _strlen_recursion((s + 1)));
 }
 return (0);
 }
