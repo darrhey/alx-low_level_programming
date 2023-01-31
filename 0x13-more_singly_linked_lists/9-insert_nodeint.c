32a372ed421c1b7b301a400e7b7aa9b40f7ef9d1
Elegbede Oludare <elegbededare@gmail.com>
2023-01-31 16:01:01 +0100

    insert nodeint

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,12 +11,12 @@ static listint_t *new(const int n);
  * Return: the address of new node or NULL if failed
  */
 
-listint_t *insert_nodeint_at_index(listint_t *head, unsigned int idx, int n)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 unsigned int i;
 i = 0;
 listint_t *node, *tmp;
-if (!(*head) && idx == 0)
+if (!(*head) && (idx == 0))
 {
 *head = new(n);
 return (*head);
