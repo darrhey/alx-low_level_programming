543a662bdd399fd8813cb79ef2d495060e21b77a
Elegbede Oludare <elegbededare@gmail.com>
2023-01-31 16:57:01 +0100

    insert_nodeint

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,39 +2,60 @@
 #include <stdlib.h>
 #include "lists.h"
 
+static listint_t *new(const int n);
+
 /**
  * insert_nodeint_at_index - inserts a new node at a position
  * @head: pointer to a listint_t list
  * @idx: index to insert node
- * @n: int type
- * Return: the address of new node or NULL if failed
+ * Return: address of the new node or NULL if failed
  */
 
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+int *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+unsigned int i;
 listint_t *nodes, *tmp;
-nodes = *head;
+if (!(*head) && idx == 0)
+{
+*head = new(n);
+return (*head);
+}
+i = 0;
+tmp = *head;
+while (tmp)
+{
+if (idx == 0)
+{
+*head = new(n);
+(*head)->next = tmp;
+return (*head);
+}
+else if (i == (idx - 1))
+{
+nodes = new(n);
+nodes->next = tmp->next;
+tmp->next = nodes;
+return (nodes);
+}
+tmp = tmp->next;
+i++;
+}
+return (NULL);
+}
+
+/**
+ * new - creates a new node
+ * @n: int type
+ * return: pointer to the new node
+ */
+
+static listint_t *new(const int n)
+{
+listint_t *tmp;
 tmp = malloc(sizeof(listint_t));
 if (!tmp)
 return (NULL);
 tmp->n = n;
-if (idx == 0)
-{
-tmp->next = node;
-*head = tmp;
-return (*head);
-}
-while (idx > 1)
-{
-nodes = nodes->next;
-index--;
-if (!nodes)
-{
-free(tmp);
-return (NULL);
-}
-}
-tmp->next = nodes->next;
-node->next = tmp;
+tmp->next = NULL;
 return (tmp);
 }
