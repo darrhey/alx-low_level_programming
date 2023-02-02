2c8826c3e8c9d6cb96e23d71a482192a8fc2c4bd
Elegbede Oludare <elegbededare@gmail.com>
2023-02-02 12:41:06 +0100

    binary to unit

---
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * binary_to_unit - converts a binary number to an unsigned int
+ * @b: string to be converted
+ * Return: the converted number or 0 if b contains value other than 0 or 1
+ */
+unsigned int binary_to_unit(const char *b)
+{
+unsigned int i, j;
+if (!b || !*b)
+return (0);
+j = 0;
+i = j;
+while (b[j])
+{
+if (b[j] > 49)
+return (0);
+else if (b[j] == 49)
+{
+i <<= 1;
+i += 1;
+}
+else
+{
+i <<=1;
+}
+j++;
+}
+return (i);
+}
