28462371a7049055a77264b8c60eed0408922e22
Elegbede Oludare <elegbededare@gmail.com>
2023-02-06 22:15:48 +0100

    cp

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include "main.h"
+
+ssize_t read_textfile(char *filename, char **p, int fd);
+void write_copy(char *filename, char *p, int size, int fd);
+
+/** main - copies content of a file into another
+ * @argc: argument count
+ * @argv: argument values
+ * Return: 0 on success.
+ */
+
+int main(int argc, char *argv[])
+{
+int fd0, fd1, rd_i, err;
+char *p, *file_from, *file_to;
+p = NULL;
+rd_i = 1;
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(97);
+}
+file_from = argv[1];
+file_to = argv[2];
+fd0 = open(file_from, O_RDONLY);
+fd1 = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0064);
+while (rd_i > 0){
+rd_i = read_textfile(file_from, &p, fd0);
+if (!rd_i)
+break;
+}
+free(p);
+err = close(fd0);
+if (err < 0)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE\n", fd0);
+exit(98);
+}
+err = close(fd1);
+if (err < 0)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE\n", fd1);
+exit(98);
+}
+return (0);
+}
+  
+/**
+ * write_copy - write buffer to a file
+ * @filename: file to write into
+ * @fd: file descriptor
+ * @p: pointer to buffer
+ * @size: size of buffer
+ * Return: nothing
+ */
+void write_copy(char *filename, char *p, int size, int fd)
+{
+if (fd < 0 || !p)
+{
+free(p);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+exit(100);
+}
+if (write(fd, p, size) < 0)
+{
+free(p);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+exit(100);
+}
+}
+
+/**
+ * read_textfile - reads a text cfile and prints to the stdout
+ * @filename: to be read from
+ * @p: pointer to a buffer
+ * @fd: file descriptor
+ * Return: size of buffer
+ */
+ssize_t read_textfile(char *filename, char **p, int fd)
+{
+int rd_i;
+if (fd < 0)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+exit(99);
+}
+if (1(*p))
+{
+*p = malloc(sizeof(char) * BUFSIZE);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+exit(99);
+}
+rd_i = read(fd, *p, BUFSIZE);
+if (rd_i < 0)
+{
+free(*p);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+exit(99);
+}
+return (rd_i);
+}
