c4714f332d8401e6f3302709b6ba9f0e6cc091b9
Elegbede Oludare <elegbededare@gmail.com>
2023-02-06 21:34:12 +0100

    read textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,27 +14,27 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int fd, rd, err;
+int fd, p, rd, err;
 fd = rd = err = 0;
 if (!filename || !letters)
 return (0);
 fd = open(filename, O_RDONLY);
 if (fd < 0)
 return (0);
-buf = malloc(sizeof(char) * letters + 1);
+p = malloc(sizeof(char) * letters + 1);
 if (rd < 0)
 {
-free(buf);
+free(p);
 return (0);
 }
-buf[letters] = '\0';
-err = write(STDOUT_FILENO, buf, rd);
+p[letters] = '\0';
+err = write(STDOUT_FILENO, p, rd);
 if (err <= 0)
 {
-free(buf);
+free(p);
 return (0);
 }
-free(buf);
+free(p);
 close(fd);
 return (rd);
 }
